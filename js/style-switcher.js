78ef6d590639095de6a58cda12cc6af91f548638
Guitsdriiy <guilhermerique2018@hotmail.com>
2023-02-16 17:23:06 -0300

    commit

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -23,14 +23,14 @@ function setActiveStyle(color) {
 /* ======================================== theme light and dark mode =======================*/
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
   dayNight.querySelector("i").classList.toggle("fa-moon");
+  dayNight.querySelector("i").classList.toggle("fa-sun");
   document.body.classList.toggle("dark");
 });
 window.addEventListener("load", () => {
   if (document.body.classList.contains("dark")) {
-    dayNight.querySelector("i").classList.add("fa-sun");
-  } else {
     dayNight.querySelector("i").classList.add("fa-moon");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-sun");
   }
 });
